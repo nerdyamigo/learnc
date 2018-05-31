e01267dfbde3d4602ea7ccfa89f1b6986c1beb97
cyberducky <czavaleta@nerdyamigo.com>
2018-05-31 19:00:29 +0000

    Adding debug header, and changes to the ex1 folder

--- dbg.h
+++ dbg.h
@@ -15,7 +15,25 @@
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
 #define log_err(M, ...) fprintf(sterr, \
-"[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, \
+	"[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, \
 cleanerrno(), ##__VA_ARGS__)
+
+#define log_warm(M, ...) fprintf(stderr, \
+	"[WARN] (%s:%d: errno: %s) " M "\n", \
+	__FILE__, __LINE__, clean_errno(), ##__VAR_ARGS__)
+
+#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n", \
+	__FILE__, __LINE__, ##__VA_ARGS__)
+
+#define check(A, M, ...) if(!(A)) {\
+	log_err(M, ##__VA_ARGS__); errno=0; goto error}
+
+#define sentinel(M, ...) { log_err(M, ##__VA_ARGS__);\
+	errno=0; goto error;}
+
+#define check_debug(A, M, ...) if(!(A)) { debug(M, ##__VA_ARGS__); \
+	errno=0; goto error;}
+
+#endif
 
 
---
+++ ex1/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char *argv[])
+{
+    
+    // this is also a comment
+    printf("Hello World!\n");
+    
+    return 0;
+}
